f25bf8b1e2a9c4f1e02bf82eb19512401ef575b2
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-09-05 19:56:42 +0900

    Section 3: Getting Started with Async and Await

--- GettingStartedAsyncAwait/ContentView.swift
+++ GettingStartedAsyncAwait/ContentView.swift
@@ -7,10 +7,62 @@
 
 import SwiftUI
 
+struct CurrentDate: Decodable, Identifiable {
+    let id = UUID()
+    let date: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case date = "date"
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var currentDates: [CurrentDate] = []
+    
+    private func getDate() async throws -> CurrentDate? {
+        
+        guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
+            fatalError("Url is incorrect!")
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try? JSONDecoder().decode(CurrentDate.self, from: data)
+    }
+    
+    private func populateDates() async {
+        
+        do {
+            guard let currentDate = try await getDate() else {
+                return
+            }
+            
+            self.currentDates.append(currentDate)
+            
+        } catch {
+            print(error)
+        }
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(currentDates) { currentDate in
+                Text("\(currentDate.date)")
+            }.listStyle(.plain)
+            
+            .navigationTitle("Dates")
+            .navigationBarItems(trailing: Button(action: {
+                // button action
+                Task.init {
+                    await populateDates()
+                }
+            }, label: {
+                Image(systemName: "arrow.clockwise.circle")
+            }))
+            .task {
+                await populateDates()
+            }
+        }
     }
 }
 
